d6fa84ca74060a82b04dc0c5b3a2cfce0cb39eae
Alejandrorodram <ale1.zul@gmail.com>
2022-03-04 11:44:18 +0000

    first commit

---
+++ EvenOrOdd/EvenOrOdd.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvenOrOdd
+{
+    internal class EvenOrOdd
+    {
+        public static string EvenOrOdd1(int number)
+        {
+            // Code goes here...
+            if (number % 2 == 0)
+            {
+                return ("Even");
+            }
+            else
+            {
+                return ("Odd");
+            }
+        }
+        public static string EvenOrOdd2(int number)
+        {
+            return number % 2 == 0 ? "Even" : "Odd";
+        }
+        public static string EvenOrOdd3(int number)
+            => number % 2 == 0 ? "Even" : "Odd";
+
+    }
+}
---
+++ EvenOrOdd/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EvenOrOdd
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(EvenOrOdd.EvenOrOdd1(5));
+            Console.WriteLine(EvenOrOdd.EvenOrOdd2(5));
+            Console.WriteLine(EvenOrOdd.EvenOrOdd3(5));
+            Console.WriteLine(EvenOrOdd.EvenOrOdd1(26));
+            Console.WriteLine(EvenOrOdd.EvenOrOdd2(26));
+            Console.WriteLine(EvenOrOdd.EvenOrOdd3(26));
+
+        }
+    }
+}
